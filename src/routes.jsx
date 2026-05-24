06e8b4084135e507cc2c3c8ccb56b354f841ab91
Dmytro Odnoroh <fe.odnoroh.dmytro@gmail.com>
2026-05-24 21:23:52 +0300

    task 2 completed, top 50 crypto here (as a primary color, sry:(

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,18 @@
-import { HomeOutlined } from "@ant-design/icons";
+import { AreaChartOutlined, HomeOutlined } from "@ant-design/icons";
 import Home from "./pages/Home.jsx";
+import Coins from "./pages/Coins.jsx";
 
 export const routes = [
-    { path: "/", label: "Home", icon: <HomeOutlined />, element: <Home /> },
+  {
+    path: "/",
+    label: "Home",
+    icon: <HomeOutlined />,
+    element: <Home />
+  },
+  {
+    path: "/coins",
+    label: "Quote",
+    icon: <AreaChartOutlined />,
+    element: <Coins />,
+  }
 ];
